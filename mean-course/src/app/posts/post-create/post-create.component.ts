b2c9e355ad168c3cf76ad2fa69134ebeafd6f9f5
Leonard Landicho <leonard_landicho@yahoo.com>
2020-05-05 13:57:06 -0400

    commit step 2.11.5

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, EventEmitter, Output  } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-post-create',
@@ -7,21 +9,20 @@ import { Component, OnInit, EventEmitter, Output  } from '@angular/core';
 })
 export class PostCreateComponent implements OnInit {
   constructor() { }
-  
-  @Output() postCreated = new EventEmitter();
 
+  @Output() postCreated = new EventEmitter<Post>();
   ngOnInit(): void {
   }
-  
   enteredTitle = '';
   enteredContent = '';
-  onAddPost(){   
-    const post = {
-      title: this.enteredTitle, 
-      content:this.enteredContent
+  onAddPost(form: NgForm){   
+    if(form.invalid){
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content:form.value.content
     }
     this.postCreated.emit(post);
-    //alert("this is the the title:" + this.enteredTitle);
   }
-
 }
